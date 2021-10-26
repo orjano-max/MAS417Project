6b4bf0f61ed966db9474cc6ebc9f7e1ba635a1bd
Ørjan Øvsthus <orjano18@uia.no>
2021-10-26 16:32:03 +0200

    Added Weather funcionality
    Can now check locations and get the weather

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 # This is the main file for the MAS417 Project
 
-import STLGenerator as generator
+# import STLGenerator as generator
 from Weather import Weather
-import PictureProcessing
+# import PictureProcessing
 
 
 def main():
-    print("Hello World")
+
+
+    # --------------------------------------------------------------------
+    # This part is used to test the weather class. Currently commented out
+    """
+    # Initiate weather and save the location in the weather class
+    weather = Weather(input("Enter Location: "))
+
+    # Check if the location exists
+    for attempt in range(5):
+        location_exists = weather.check_location()
+        if location_exists:
+            # The location exists!
+            break
+        else:
+            print("This location does not exist.\n Try Again!")
+            weather.set_location()
+    else:
+        print('No more tries.\n Goodbye!')
+
+    print(weather.get_weather())
+
+    #print(weather)
+    """
+    # ----------------------------------------------------------------------------
 
 
 if __name__ == "__main__":
